2b5dbf836084af39beb42a84acaa770667bd076e
Priyam Vaidya <priyam304@gmail.com>
2019-03-07 14:38:14 +0530

    Helper method structure

---
+++ issueone/helpers.py
@@ -0,0 +1,19 @@
+
+def language(lang_name):
+    return
+
+
+def user(username):
+    return
+
+
+def repository(repo_name):
+    return
+
+
+def topic(topic_name):
+    return
+
+
+def search(type, search_param):
+    return
